1e49f9e0d104c7cf53534264baeca579c1f7276b
jay1535 <habbujayanth@gmail.com>
2026-02-25 22:03:18 +0530

    Added Custom Auth Pages

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -10,12 +10,9 @@ import { Button } from "./ui/button";
   export default function Header() {
     return (
       <header className="
-     fixed top-0 w-full border-b border-stone-200 bg-stone-50/80 backdrop-blur-md z-50 supports-backdrop-filter:bg-stone-50/60
-      ">
-        <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
-          
+     fixed top-0 w-full border-b border-stone-200 bg-stone-50/80 backdrop-blur-md z-50 supports-backdrop-filter:bg-stone-50/60">
+        <nav className="container mx-auto px-4 h-16 flex items-center justify-between"> 
          LOGO
-
         <div>
           Nav Links
           </div>
@@ -46,10 +43,7 @@ import { Button } from "./ui/button";
               </Button>
             </SignUpButton>
           </SignedOut>
-  
-           
           </div>
-  
         </nav>
       </header>
     );
